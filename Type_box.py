8a9be20f273f4ffdfe87d93ce24830101da64a0c
oved01 <66124214+oved01@users.noreply.github.com>
2021-03-18 09:55:47 +0100

    affichage

--- Type_box.py
+++ Type_box.py
@@ -2,10 +2,13 @@
 # -*- coding: UTF-8 -*-
 
 
-class Type_box(object): 
-    def __init__(self, t, h, l, p):
-        self.___type = t
-        self.___hbox = h
-        self.___lbox = l
-        self.___prix_box = p
+class Type_box(object):
 
+    def __init__(self, t, h, l, p):
+        self.type = t
+        self.hbox = h
+        self.lbox = l
+        self.prix_box = p
+
+    def affichage(self):
+        print ("Type_box", self.type, " ", self.hbox, " ", self.lbox, " ", self.prix_box)
