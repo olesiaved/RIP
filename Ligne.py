641407f3aa536f8548b754ff15f3c51f96e38620
oved01 <66124214+oved01@users.noreply.github.com>
2021-03-18 10:06:54 +0100

    affichage

--- Ligne.py
+++ Ligne.py
@@ -6,10 +6,11 @@ from Produit import Produit
 class Ligne(object):
     def Produire(self, aDate : int) -> None:
         pass
-    def __init__(self):
-        self.___numero : int = None
+    def __init__(self,n):
+        self.___numero=n
         self.___produit_en_Cours : Produit = None
         self.___dernier_Produit_fini : Produit = None
         self._listes_commandes = []
-
+    def affichage(self):
+        print ("Ligne", self.___numero)
 
