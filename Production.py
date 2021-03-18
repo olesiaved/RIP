b05be72d197fe8f62c01f3a6f0cb9f754fc55a51
oved01 <66124214+oved01@users.noreply.github.com>
2021-03-18 10:32:24 +0100

    yees

--- Production.py
+++ Production.py
@@ -4,27 +4,41 @@ from Box_manager import Box_manager
 from Scheduling import Scheduling
 from Commande import Commande
 from Type_produit import Type_produit
+from Ligne import Ligne
 from Type_box import Type_box
-import os
-import csv
 
 class Production(object):
+    def simulation_production(self,filename):
+        self.Lecture_fichier(filename)
+        self.create_Lignes()
+        self.create_Commandes()
+        self.create_Type_box()
+        self.create_Type_produit()
     def Calcul_cout_delay(self, aCmd : Commande):
         pass
 
-    def Lecture_fichier(self):
+    def Lecture_fichier(self,filename):
         production_list=[]
-        dir = os.path.dirname(__file__)
-        filename = os.path.join(dir, './Donnee1.txt')
         with open(filename, newline='') as csvfile:
             for line in csvfile:
                 production_list.append(line.strip().split())
             self._lecture_txt=production_list
+
     def create_Type_produit(self):
         list_Type_produit=[]
         for i in range (int(self._lecture_txt[0][0])):
             list_Type_produit.append(Type_produit(self._lecture_txt[i+1][0], int(self._lecture_txt[i+1][1]),int(self._lecture_txt[i+1][2]),int(self._lecture_txt[i+1][3]),int(self._lecture_txt[i+1][4]), int(self._lecture_txt[i+1][5]) ))
         self.box_manager._listes_types_produit=list_Type_produit
+
+    def create_Commandes(self):
+        list_Commande = []
+        a = int(self._lecture_txt[0][0])
+        b = int(self._lecture_txt[0][1])
+        for i in range(a, a + b):
+            list_Commande.append(Commande(self._lecture_txt[i + 1][0], int(self._lecture_txt[i + 1][1]),
+                                          int(self._lecture_txt[i + 1][2]), int(self._lecture_txt[i + 1][3])))
+        self._schedule._liste_commandes=list_Commande
+
     def create_Type_box(self):
         list_Type_box=[]
         a=int(self._lecture_txt[0][0])
@@ -32,9 +46,14 @@ class Production(object):
         c=int(self._lecture_txt[0][3])
         for i in range(a+b,a+b+c):
             list_Type_box.append(Type_box(self._lecture_txt[i+1][0], int(self._lecture_txt[i+1][1]),int(self._lecture_txt[i+1][2]),int(self._lecture_txt[i+1][3]) ))
-        return list_Type_box
+        self.box_manager._listes_types_box=list_Type_box
 
-            
+    def create_Lignes(self):
+        list_Lignes = []
+        c = int(self._lecture_txt[0][2])
+        for i in range(c):
+            list_Lignes.append(Ligne(i))
+        self._schedule._liste_lignes=list_Lignes
 
     def Ecriture_fichier(self):
         pass
@@ -45,13 +64,10 @@ class Production(object):
     def Calcul_cout_conteneur(self):
         pass
 
-    def Calcul_nbdeBoxes(self):
-        pass
-
     def __init__(self):
         self._cout = 1
         self.box_manager=Box_manager()
         """# @AssociationMultiplicity 1"""
-        self._schedule : Scheduling = None
+        self._schedule=Scheduling()
         """# @AssociationMultiplicity 1"""
         self._lecture_txt=None
